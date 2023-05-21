fb163d97deb85fb6bc113ee0f373dc20aab61eb8
victorBuzdugan <buzdugan.victor@icloud.com>
2023-05-21 11:22:22 +0300

    got search results

--- filelist.py
+++ filelist.py
@@ -3,7 +3,8 @@
 # FileList.io search engine plugin for qBittorrent
 # [x] login
 # [x] test login errors
-# [ ] get search results
+# [x] get search results
+# [x] get individual results from search results
 # [ ] parse torrent data from search results
 # [ ] return to qBitTorrent data
 # [ ] get search results from all pages
@@ -32,6 +33,12 @@ USER_AGENT: tuple = ('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15
 # region: regex patterns
 # get validator key from login page 
 RE_VALIDATOR = re.compile(r"(?<=name='validator' value=')(.*)(?=' \/>)")
+RE_ALL_RESULTS = re.compile(
+    r"""
+    <div\sclass='torrentrow'>                    # starts with
+    [\S\s]*?                                    # any char, including new line
+    <div\sclass='clearfix'><\/div>\s*<\/div>     # ends with
+    """, re.VERBOSE)
 # endregion
 
 # region: login credentials
@@ -59,13 +66,16 @@ class filelist:
     url_dl = url + 'download.php?id='
     url_login = url + 'login.php'
     url_login_post = url + 'takelogin.php'
+    url_search = url + 'browse.php?search'
     url_details = url + 'details.php?id='
     supported_categories = {
         'all': '0',
-        'software': '8',
-        'games': '9',
+        'movies': '19',
+        'tv': '21',
         'music': '11',
-        'anime': '24'
+        'games': '9',
+        'anime': '24',
+        'software': '8'
     }
     
     # initialize cookie jar
@@ -106,7 +116,6 @@ class filelist:
 
         # POST form and login
         main_page = self._make_request(self.url_login_post, data=self.payload)
-        pprint(main_page)
 
     def _make_request(self, url: str, data=None) -> Optional[str]:
         ''' GET and POST to 'url'.
@@ -136,6 +145,8 @@ class filelist:
             if error.code == 403:
                 print('Bad "user-agent". Header not loaded. '
                       'Connection refused!')
+            if error.code == 404:
+                print('404 Page not found!')
             # print(error.code, error.reason)
             pass
         except URLError as error:
@@ -157,7 +168,7 @@ class filelist:
 
     # DO NOT CHANGE the name and parameters of this function
     # This function will be the one called by nova2.py
-    def search(self, what, cat='all'):
+    def search(self, what: str, cat='all') -> None:
         """
         Here you can do what you want to get the result from the search engine website.
         Everytime you parse a result line, store it in a dictionary
@@ -167,5 +178,62 @@ class filelist:
         `cat` is the name of a search category in ('all', 'movies', 'tv', 'music', 'games', 'anime', 'software', 'pictures', 'books')
         """
 
+        if cat not in self.supported_categories:
+            # print(f'Category "{cat}" not found, defaulting to "all".')
+            cat = 'all'
+
+        # create search url
+        search_link = [
+            # ('https://filelist.io/browse.php?search', search_string)
+            (self.url_search, what),
+            # category
+            ('cat', self.supported_categories[cat]),
+            # 0: Name & Description, 1: Name, 2: Description, 3: IMDB id
+            ('searchin', '1'),
+            # 0: Hybrid, 1: Relevance, 2: Date, 3: Size, 
+            # 4: Downloads, 5:Seeders
+            ('sort', '5')
+        ]
+        # print('Before encoding url: ', search_link)
+        search_link = urlencode(search_link, safe=':/?+')
+        # print('Encoded url: ', search_link)
+
+        search_results_page = self._make_request(search_link)
+        if 'Rezultatele cautarii dupa' in search_results_page:
+            # retutned results page
+            torrent_row = re.finditer(RE_ALL_RESULTS, search_results_page)
+
+        else:
+            # print('Cannot access search results page!')
+            pass
+
+# You must pass to this function a dictionary containing the following keys
+# (value should be -1 if you do not have the info):
+
+# [ ] link => A string corresponding the the download link (the .torrent file or magnet link)
+# [ ] name => A unicode string corresponding to the torrent's name (i.e: "Ubuntu Linux v6.06")
+# [ ] size => A string corresponding to the torrent size (in bytes???) (i.e: "6 MB" or "200 KB" or "1.2 GB"...)
+# [ ] seeds => The number of seeds for this torrent (as a string)
+# [ ] leech => The number of leechers for this torrent (a a string)
+# [ ] engine_url => The search engine url (i.e: http://www.mininova.org)
+# [ ] desc_link => A string corresponding to the the description page for the torrent
+
+
+
 if __name__ == "__main__":
     a = filelist()
+    a.search('mandalorian+s01', 'tv')
+    pass
+
+# with open('FileListResults.html') as file:
+#     a = file.read()
+
+# # print(a)
+
+
+# b = re.finditer(RE_ALL_RESULTS, a)
+
+
+# for i in b:
+#     print(i.group())
+
